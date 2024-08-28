f97af36850dcc5b4c5d3d471092e2c2bc751b732
DHINESH00 <18csa09@karpagamtech.ac.in>
2024-08-28 11:00:12 +0530

    ref:add Estimation Detail make "Start Date" of the first task will be the "End Date" + 1 from the previously present task under that project

--- project_management/api.py
+++ project_management/api.py
@@ -1,19 +1,26 @@
 import frappe, json
 from frappe import _
+from datetime import datetime ,timedelta
 @frappe.whitelist()
-def get_tasks_from_multiple_template(template,name,parent_task=None):
+def get_tasks_from_multiple_template(name,tasks,parent_task=None,):
     try:
+        tasks=json.loads(tasks)
         doc = frappe.get_doc("Project",name)
         project_tasks = []
         tmp_task_details = []
-        tasks =frappe.get_all("Project Template Task",filters= { "parent": template }, fields= 'task',pluck="task")
+        
         for task in tasks:
-            template_task_details = frappe.get_doc("Task", task)
+            template_task_details = frappe.get_doc("Task", task.get("task"))
             tmp_task_details.append(template_task_details)
+            print(template_task_details.__dict__)
             task_doc = doc.create_task_from_template(template_task_details)
+            if task.get("start_date"):
+                task_doc.exp_start_date = task.get("start_date")
+            if task.get("end_date"):
+                task_doc.exp_end_date = task.get("end_date")
             if parent_task:
                 task_doc.parent_task=parent_task
-                task_doc.save()
+            task_doc.save()
             project_tasks.append(task_doc)
         doc.dependency_mapping(tmp_task_details, project_tasks)
         if len(project_tasks):
@@ -22,3 +29,38 @@ def get_tasks_from_multiple_template(template,name,parent_task=None):
         frappe.log_error(title="Get Tasks From Multiple Template",message=frappe.get_traceback())
         frappe.msgprint(_(str(e)))
     return False
+@frappe.whitelist()
+def get_task_value(template,project,company):
+    last_exp_date=frappe.db.get_value("Task",{"project":project},"exp_start_date",order_by="creation")
+    
+    
+    tasks =frappe.get_all("Project Template Task",filters= { "parent": template }, fields= ["name",'task',"subject"])
+    for task in tasks:
+        last_exp_date=next_woking_date(last_exp_date,company)
+        task ['start_date']=str(last_exp_date)
+        task["end_date"]=str(last_exp_date)
+    return tasks
+@frappe.whitelist()
+def next_woking_date(date,company):
+    exp_start_date = datetime.strptime(str(date), '%Y-%m-%d')  if str(type(date)) =="<class 'datetime.date'>" else datetime.strptime(str(date), '%Y-%m-%d %H:%M:%S')
+    exp_start_date = exp_start_date + timedelta(days=1)
+    today = datetime.today()
+    if exp_start_date < today:
+        exp_start_date = today
+    default_holiday_list=frappe.db.get_value("Company",company,"default_holiday_list")
+    if default_holiday_list:
+        to_date=frappe.db.get_value("Holiday List",default_holiday_list,"to_date")
+        to_date=datetime.strptime(str(to_date), '%Y-%m-%d')
+        if to_date >= exp_start_date:
+            leave_date=frappe.get_all("Holiday",{"parent":default_holiday_list},"holiday_date",pluck="holiday_date")
+            if len(leave_date):
+                while True:
+                    if exp_start_date in leave_date:
+                        exp_start_date = exp_start_date + timedelta(days=1)
+                    else:
+                        break
+        else:
+            frappe.throw("Default holiday Not in range for  Company  "+company +" \n got date "+str(exp_start_date))
+    else:
+        frappe.throw("Please set default holiday list in Company "+company)
+    return exp_start_date 
--- project_management/public/js/project.js
+++ project_management/public/js/project.js
@@ -2,7 +2,86 @@ frappe.ui.form.on('Project', {
 	refresh:function(frm){
 	    if(!frm.doc.__islocal){
 	        frm.add_custom_button(__("Add Tasks from Template"), () => {
-	       
+				var fields = [
+					{
+						fieldtype: "Data",
+						fieldname: "name",
+						read_only: 1,
+						hidden: 1,
+					},
+					{
+						fieldname: "subject",
+						fieldtype: "Data",
+						in_list_view: 1,
+						read_only: 1,
+						label: "Subject"
+					   },
+					{
+						fieldtype: "Link",
+						fieldname: "task",
+						read_only: 1,
+						in_list_view: 1,
+						label: __("Template Task"),
+						options: "Task",
+					},
+					
+					{
+						fieldtype: "Date",
+						fieldname: "start_date",
+						label: __("Start Date"),
+						in_list_view: 1,
+						default: frappe.datetime.nowdate(),
+					},
+					{
+						fieldtype: "Date",
+						fieldname: "end_date",
+						in_list_view: 1,
+						label: __("End Date"),
+						read_only: 1,
+						// onchange: function () {
+						// 	debugger
+						// 	var task_table_data =d.get_value("task_table");
+						// 	if (task_table_data.length > 0) {
+						// 		var frist_row_endate=task_table_data[0].end_date
+						// 		 frist_row_endate = new Date(frist_row_endate);
+						// 	for (let index = 1; index < task_table_data.length; index++) {
+						// 		var start_date=task_table_data[index].start_date
+						// 		start_date = new Date(start_date);
+						// 		if(start_date <= frist_row_endate){
+						// 			frappe.call({
+						// 				method: "project_management.api.next_woking_date",
+						// 				args: {
+						// 					date: `${start_date.getFullYear()}-${String(start_date.getMonth()).padStart(2, '0')}-${String(start_date.getDate()).padStart(2, '0')}`,
+						// 					company:frm.doc.company
+						// 				},
+						// 				callback: function (r) {
+						// 					if (!r.exc) {
+												
+						// 						if (r.message) {
+						// 							d.fields_dict.task_table.df.data[index].start_date =r.message
+	
+						// 							d.fields_dict.task_table.refresh()
+						// 						}
+						// 					}
+						// 				},
+						// 			})
+
+									
+						// 		}
+						// 		frist_row_endate = new Date(task_table_data[index].end_date);
+								
+								
+						// 	}
+								
+						// 	}
+							
+
+						// },
+						// default: frappe.datetime.nowdate(),
+
+					},
+				]
+				var data=[];
         	        var d = new frappe.ui.Dialog({
         			title: __("Add Multiple Templates"),
         			fields: [
@@ -11,7 +90,31 @@ frappe.ui.form.on('Project', {
         					fieldname: "template",
         					fieldtype: "Link",
         					options:"Project Template",
-        					reqd:1
+        					reqd:1,
+							change: () => {
+								debugger
+								var template=d.get_value("template");
+								frappe.call({
+									method: "project_management.api.get_task_value",
+									args: {
+										template:template,
+										project:frm.doc.name,
+										company:frm.doc.company
+									},
+									callback: function (r) {
+										// debugger
+										if (!r.exc) {
+											
+											if (r.message) {
+												d.fields_dict.task_table.df.data =r.message
+
+												d.fields_dict.task_table.refresh()
+											}
+										}
+									},
+								})
+
+							}
         					
         				},
 						{
@@ -32,7 +135,19 @@ frappe.ui.form.on('Project', {
 							}
 							
         					
-        				}
+        				},
+						{
+							fieldname: "task_table",
+							fieldtype: "Table",
+							label: "Tasks Update Values",
+							 cannot_add_rows: true,
+							in_place_edit: true,
+							depends_on: "eval:doc.template",
+							fields: fields,
+							
+							reqd:1,
+
+						},
         			],
         			primary_action: function () {
         				var data = d.get_values();
@@ -41,7 +156,9 @@ frappe.ui.form.on('Project', {
             					args: {
             						template:data.template,
 									parent_task:data.parent_task,
-            						name:frm.doc.name
+            						name:frm.doc.name,
+									tasks:data.task_table
+									
             					},
             					callback: function (r) {
             						if (!r.exc) {
